434134bf49ba194c6e76968ab0444d6b30844766
Eduardo <user_mrttgvtef311@tribanco.com.br>
2022-10-17 17:55:39 -0300

    Ajuste

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ import java.net.http.HttpResponse;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String url = "https://imdb-api.com/API/Search/k_puf8lzus/leon%20the%20professional/";
-        URI endereco = URI.create(url);
+
+        var url = "https://imdb-api.com/API/Search/k_puf8lzus/leon%20the%20professional/";
+        var endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
-        //resposta string
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         String body = response.body();
