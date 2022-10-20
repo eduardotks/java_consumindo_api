5c8971c41e0375080b44938cdeaa7261b63ecd37
Eduardo <user_mrttgvtef311@tribanco.com.br>
2022-10-20 14:04:01 -0300

    Mesclagem de Gerador de figurinhas com a Main

--- src/GeradorDeFigurinhas.java
+++ src/GeradorDeFigurinhas.java
@@ -5,15 +5,16 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 public class GeradorDeFigurinhas {
-    public void Cria() throws IOException {
+    public void cria(InputStream inputStream, String NomeArquivo ) throws IOException {
         //por link
-        URL inputS = new URL("https://img.freepik.com/vetores-gratis/fundo-azul-favo-de-mel-tecnologico_52683-22308.jpg");
+        //URL inputS = new URL("https://img.freepik.com/vetores-gratis/fundo-azul-favo-de-mel-tecnologico_52683-22308.jpg");
         //por diretório
         //BufferedImage imagemOriginal = ImageIO.read(new File("entrada/teste1.jpg"));
-        BufferedImage imagemOriginal = ImageIO.read(inputS);
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int NovaAltura = altura + 100;
@@ -21,18 +22,18 @@ public class GeradorDeFigurinhas {
         Graphics2D graph = (Graphics2D) novaImagem.getGraphics();
         graph.drawImage(imagemOriginal, 0, 0, null);
         //--- altera fonte
-        Font fonts = new Font(Font.SANS_SERIF, Font.BOLD, 40);
+        Font fonts = new Font(Font.SANS_SERIF, Font.BOLD, 20);
         graph.setColor(Color.ORANGE);
         graph.setFont(fonts);
-        graph.drawString("TESTE TESTE", 160, NovaAltura - 60 );
+        graph.drawString(NomeArquivo, 160, NovaAltura - 60 );
         //---
         //local final
-        ImageIO.write(novaImagem, "png", new File("saida/final2.jpg"));
+        ImageIO.write(novaImagem, "png", new File("saida/" + NomeArquivo + ".png"));
     }
 
     //para rodar basta executar esta função main e observar a pasta saída.
-    public static void main(String[] args) throws IOException {
-        var geradora =  new GeradorDeFigurinhas();
-        geradora.Cria();
-    }
+    //public static void main(String[] args) throws IOException {
+    //var geradora =  new GeradorDeFigurinhas();
+    //geradora.cria();
+    //}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -9,7 +11,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        var url = "https://imdb-api.com/API/Search/k_puf8lzus/leon%20the%20professional/";
+        var url = "https://imdb-api.com/en/API/Top250Movies/k_puf8lzus/";
         var endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -22,16 +24,19 @@ public class Main {
         List<Map<String,String>> listItens = p.parse(body);
 
         for(Map<String,String> filme : listItens){
+            String urlImg = filme.get("image");
+            String nomeArquivo = filme.get("title");
+            InputStream inputStream = new URL(urlImg).openStream();
+
+            var geradora = new GeradorDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
             System.out.println(filme.get("id"));
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
         }
+
     }
 }
-
-
-
-
 
 
 
