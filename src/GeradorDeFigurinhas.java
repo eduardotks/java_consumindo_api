5a9fb071c45360770b4d3e991ac1cd87e0690ba8
Eduardo <user_mrttgvtef311@tribanco.com.br>
2022-10-20 10:14:07 -0300

    Correção de código // ADD uma forma de pegar imagem através de links.

--- src/GeradorDeFigurinhas.java
+++ src/GeradorDeFigurinhas.java
@@ -5,27 +5,32 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class GeradorDeFigurinhas {
     public void Cria() throws IOException {
-        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/teste1.jpg"));
+        //por link
+        URL inputS = new URL("https://img.freepik.com/vetores-gratis/fundo-azul-favo-de-mel-tecnologico_52683-22308.jpg");
+        //por diretório
+        //BufferedImage imagemOriginal = ImageIO.read(new File("entrada/teste1.jpg"));
+        BufferedImage imagemOriginal = ImageIO.read(inputS);
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int NovaAltura = altura + 100;
         BufferedImage novaImagem = new BufferedImage(largura, NovaAltura, BufferedImage.TRANSLUCENT);
         Graphics2D graph = (Graphics2D) novaImagem.getGraphics();
         graph.drawImage(imagemOriginal, 0, 0, null);
-        //---
-        graph.drawString("BRABO", 100, NovaAltura - 60 );
-        Font fonts = new Font(Font.SANS_SERIF, Font.BOLD, 60);
+        //--- altera fonte
+        Font fonts = new Font(Font.SANS_SERIF, Font.BOLD, 40);
         graph.setColor(Color.ORANGE);
         graph.setFont(fonts);
+        graph.drawString("TESTE TESTE", 160, NovaAltura - 60 );
         //---
-
-        ImageIO.write(novaImagem, "png", new File("saida/final1.jpg"));
+        //local final
+        ImageIO.write(novaImagem, "png", new File("saida/final2.jpg"));
     }
 
-    //para rodar basta executar esta função main e observr a pasta saída.
+    //para rodar basta executar esta função main e observar a pasta saída.
     public static void main(String[] args) throws IOException {
         var geradora =  new GeradorDeFigurinhas();
         geradora.Cria();
